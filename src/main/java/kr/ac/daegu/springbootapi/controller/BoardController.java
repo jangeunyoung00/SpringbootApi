90b94d763e281993263e6b177c8367ebc875a2f9
jangeunyoung00 <yuzu082959@gmail.com>
2021-09-27 21:18:44 +0900

    GET/ test

---
+++ src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
@@ -0,0 +1,13 @@
+package kr.ac.daegu.springbootapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BoardController {
+
+    @RequestMapping(value = "/")
+    public  String rootTest() throws Exception{
+        return "hello!";
+    }
+}
