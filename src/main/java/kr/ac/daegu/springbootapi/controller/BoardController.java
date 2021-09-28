23e6804490826e7161576f1f28e53d893cc18ffe
jangeunyoung00 <yuzu082959@gmail.com>
2021-09-28 19:29:06 +0900

    ReeuqtMapping->GetMapping & service layer 추가

--- src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/controller/BoardController.java
@@ -1,19 +1,23 @@
 package kr.ac.daegu.springbootapi.controller;
 
 import kr.ac.daegu.springbootapi.model.TestDTO;
+import kr.ac.daegu.springbootapi.service.BoardService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController //컨트롤러 명칭
+@RequiredArgsConstructor
 @Slf4j
 public class BoardController {
 
-    @RequestMapping(value = "/")  //어노테이션을 붙여서 /를 붙였을때 본문에 해당하는 내용을 만들어 리턴시켜라
-    public List<TestDTO> rootTest() throws Exception{
+    public final BoardService boardService;
+
+    @GetMapping(value = "/")
+    public List<TestDTO> rootTest() throws Exception {
         log.trace("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~"); // 로그의 5단계 trace(일반적으로 남기는 이벤트 기록) -> debug(디버깅용) -> info(알림) -> warn(경고) -> error(에러)
         log.debug("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         log.info("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
@@ -21,10 +25,7 @@ public class BoardController {
         log.error("logTest~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
         // 간단한 list 생성
-        List<TestDTO> testList = new ArrayList<TestDTO>();
-        testList.add(new TestDTO(1, "name1"));
-        testList.add(new TestDTO(2, "name2"));
-        testList.add(new TestDTO(3, "name3name3"));
+        List<TestDTO> testList = boardService.getTestList();
 
         return testList;
     }
