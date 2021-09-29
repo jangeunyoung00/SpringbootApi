38b59e5cea7ba201cdbef19d3b5d7fa27f3e52a6
jangeunyoung00 <yuzu082959@gmail.com>
2021-09-29 21:07:58 +0900

    ApiResponse 수정

--- src/main/java/kr/ac/daegu/springbootapi/board/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/board/controller/BoardController.java
@@ -25,6 +25,6 @@ public class BoardController {
     @GetMapping(value = "/")
     public ApiResponse<BoardDTO> getBoardList() {
         List<BoardDTO> list = boardService.getBoardList();
-        return new ApiResponse("ok",list);
+        return new ApiResponse(true,list);
     }
 }
--- src/main/java/kr/ac/daegu/springbootapi/common/ApiResponse.java
+++ src/main/java/kr/ac/daegu/springbootapi/common/ApiResponse.java
@@ -4,11 +4,23 @@ import lombok.Getter;
 
 @Getter
 public class ApiResponse<T> { // wild card : 어떤 클래스던 받을 수 있다! 를 선언
-    private final String status;
-    private final T data;
+    private final boolean success;
+    private String errorMessage = null;
+    private T data;
 
-    public ApiResponse(String status, T data) {
-        this.status = status;
+    public ApiResponse(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    public ApiResponse(boolean success, String errorMessage) {
+        this.success = success;
+        this.errorMessage = errorMessage;
+    }
+
+    public ApiResponse(boolean success, String errorMessage, T data) {
+        this.success = success;
+        this.errorMessage = errorMessage;
         this.data = data;
     }
 }
