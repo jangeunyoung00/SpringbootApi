4db8990de183335f17731f88726a481d4bfd9742
jangeunyoung00 <yuzu082959@gmail.com>
2021-10-01 18:42:22 +0900

    mission 주석

--- src/main/java/kr/ac/daegu/springbootapi/board/controller/BoardController.java
+++ src/main/java/kr/ac/daegu/springbootapi/board/controller/BoardController.java
@@ -36,4 +36,12 @@ public class BoardController {
         log.debug("id: " + id);
         return boardService.putBoard(id, boardDTO);
     }
+
+    /* mission */
+    // 글 읽기
+    // 요청URL은 GET http://localhost:8080/board/{id}
+
+    // 글 삭제
+    // 요청URL은 DELETE http://localhost:8080/board/{id}
+
 }
