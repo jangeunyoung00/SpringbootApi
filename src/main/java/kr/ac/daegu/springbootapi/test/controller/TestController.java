2be97cee66ecd22fac93f5ee4f0ad0004f8a57dd
jangeunyoung00 <yuzu082959@gmail.com>
2021-10-08 20:35:02 +0900

    GET /test/dtoInList

--- src/main/java/kr/ac/daegu/springbootapi/test/controller/TestController.java
+++ src/main/java/kr/ac/daegu/springbootapi/test/controller/TestController.java
@@ -3,10 +3,13 @@ package kr.ac.daegu.springbootapi.test.controller;
 import kr.ac.daegu.springbootapi.common.ApiResponse;
 import kr.ac.daegu.springbootapi.test.model.TestDTO;
 import kr.ac.daegu.springbootapi.test.service.TestService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,6 +38,30 @@ public class TestController {
     public String insertTest(@RequestBody TestDTO testDTO) throws Exception {
         log.debug("insertTest");
         return testService.insertTest(testDTO);
-
     }
+
+    @GetMapping(value = "/dtoInList")
+    public ApiResponse<DtoInList> dtoInList() throws Exception {
+        List<TestListDTO> list = new ArrayList<TestListDTO>();
+        list.add(new TestListDTO("name1", 123));
+        list.add(new TestListDTO("name2", 2222));
+        DtoInList dtoInList = new DtoInList("hihi", 1234, "llll", list);
+        return new ApiResponse(true, dtoInList);
+    }
+}
+
+@Data
+@AllArgsConstructor
+class DtoInList {
+    private String name;
+    private int num;
+    private String name2;
+    private List<TestListDTO> list;
+}
+
+@Data
+@AllArgsConstructor
+class TestListDTO{
+    private String name;
+    private int number;
 }
